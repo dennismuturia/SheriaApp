65048359aa5c3c2346b615b078ae65a6aaced20a
dennismuturia <dmuturia2922@gmail.com>
2017-10-12 14:07:59 +0300

    This is the splash screen that is supposed to delay for 3 seconds tothe user. Ihave not yet implemented the animations but will be doing that later

--- app/src/main/java/com/sheriaapp/dennis/sheriaapp/SplashActivity.java
+++ app/src/main/java/com/sheriaapp/dennis/sheriaapp/SplashActivity.java
@@ -1,13 +1,46 @@
 package com.sheriaapp.dennis.sheriaapp;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class SplashActivity extends AppCompatActivity {
+    //This willbe the timeout time for the splash screen
+    public static int time_out = 3000;
+    @Bind(R.id.myLogo)ImageView thisLogo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //load our activity
         setContentView(R.layout.activity_splash);
+
+        ButterKnife.bind(this);
+        //This will delay our activity based on the time allocated
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+
+            public void run() {
+
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+
+                startActivity(intent);
+
+                finish();
+
+            }
+
+        }, time_out);
+
+
+
+        getSupportActionBar().hide();
+
     }
 }
