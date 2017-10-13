061175eb79c7475e14fbf78aeeeb80a878d01dc8
charliekaks <kaksunchained@gmail.com>
2017-10-13 12:38:54 +0300

    finalized the calling of data from firebase

--- app/src/main/java/com/sheriaapp/dennis/sheriaapp/MainActivity.java
+++ app/src/main/java/com/sheriaapp/dennis/sheriaapp/MainActivity.java
@@ -2,6 +2,16 @@ package com.sheriaapp.dennis.sheriaapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.sheriaapp.dennis.sheriaapp.models.Business;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +19,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
---
+++ app/src/main/java/com/sheriaapp/dennis/sheriaapp/adapters/BusinessViewHolder.java
@@ -0,0 +1,8 @@
+package com.sheriaapp.dennis.sheriaapp.adapters;
+
+/**
+ * Created by charles on 10/13/17.
+ */
+
+public class BusinessViewHolder {
+}
