d31e31a0ac385d4ad0d10fe785638d464c911cb7
dennismuturia <dmuturia2922@gmail.com>
2017-10-13 12:04:40 +0300

    Done on the MainActivity. Now i have implemented  the use of maroon color. and also made my cardviews transparent, as well as the top app bar. This are just simple styling features implemented.

--- app/src/main/java/com/sheriaapp/dennis/sheriaapp/MainActivity.java
+++ app/src/main/java/com/sheriaapp/dennis/sheriaapp/MainActivity.java
@@ -1,7 +1,13 @@
 package com.sheriaapp.dennis.sheriaapp;
 
+import android.content.Intent;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.searchmenu, menu);
+        MenuItem menuItem = menu.findItem(R.id.searchLaws);
+        final SearchView searchView = (SearchView) MenuItemCompat.getActionView(menuItem);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                String User = query;
+                Intent intent = new Intent(MainActivity.this, ListOfLaws.class);
+                intent.putExtra("User", User);
+                startActivity(intent);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+        return  super.onCreateOptionsMenu(menu);
     }
 }
