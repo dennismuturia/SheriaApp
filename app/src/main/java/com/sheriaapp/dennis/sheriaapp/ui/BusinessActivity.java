457ef29cdece77571d6b086c33d92651d237f4f8
charliekaks <kaksunchained@gmail.com>
2017-10-14 12:11:38 +0300

    Adding bottom navigation layout

--- app/src/main/java/com/sheriaapp/dennis/sheriaapp/ui/BusinessActivity.java
+++ app/src/main/java/com/sheriaapp/dennis/sheriaapp/ui/BusinessActivity.java
@@ -1,5 +1,6 @@
 package com.sheriaapp.dennis.sheriaapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,6 @@ public class BusinessActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_business);
+
     }
 }
--- app/src/main/java/com/sheriaapp/dennis/sheriaapp/ui/MainActivity.java
+++ app/src/main/java/com/sheriaapp/dennis/sheriaapp/ui/MainActivity.java
@@ -4,19 +4,27 @@ import android.content.Intent;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.SearchView;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
+    @Bind(R.id.image_business)
+    ImageView myBusiness;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        myBusiness.setOnClickListener(this);
 
     }
 
@@ -46,6 +54,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
-
+        if (view == myBusiness){
+            Intent intent = new Intent(MainActivity.this, BusinessActivity.class);
+            startActivity(intent);
+        }
     }
 }
