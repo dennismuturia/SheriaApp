db8e17d72b255cd31ef1d19ceebb9f3a9873fb74
dennismuturia <dmuturia2922@gmail.com>
2017-10-16 08:10:06 +0300

    This activity will be responsible for displaying the chat bot messages

---
+++ app/src/main/java/com/sheriaapp/dennis/sheriaapp/ui/ChatArea.java
@@ -0,0 +1,15 @@
+package com.sheriaapp.dennis.sheriaapp.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.sheriaapp.dennis.sheriaapp.R;
+
+public class ChatArea extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_area);
+    }
+}
