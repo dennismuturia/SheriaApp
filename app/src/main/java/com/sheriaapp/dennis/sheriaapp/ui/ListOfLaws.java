c26ee49d6ff0944d67b5f097af1ac4854036164a
dennismuturia <dmuturia2922@gmail.com>
2017-11-01 08:09:31 +0300

    These files are supposed to display the law of the searched laws

--- app/src/main/java/com/sheriaapp/dennis/sheriaapp/ui/ListOfLaws.java
+++ app/src/main/java/com/sheriaapp/dennis/sheriaapp/ui/ListOfLaws.java
@@ -2,10 +2,14 @@ package com.sheriaapp.dennis.sheriaapp.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 import com.sheriaapp.dennis.sheriaapp.R;
 
+import butterknife.Bind;
+
 public class ListOfLaws extends AppCompatActivity {
+    @Bind(R.id.myRecyclerView)RecyclerView myRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
